779689bfc9bc3c1a3d3f1087b01a0d691e336d40
Paulex <paulalexis.verrier@ynov.com>
2021-08-22 19:50:17 +0200

    finished portfolio

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Blog.Models;
+using System.IO;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Articles()
+        {
+            return View();
+        }
+        public IActionResult First()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+window.addEventListener('scroll', function () {
+    let nav = document.querySelector('.navbar__about');
+    windowPosition = window.scrollY > 50
+
+    nav.classList.toggle('scrolling-active', windowPosition);
+})
